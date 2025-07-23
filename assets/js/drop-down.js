52448780e64ce81e76547da7f22f9789ff1f24c3
vu1mi <phamthanhvu045@gmail.gmailcom>
2025-07-23 11:00:25 +0700

    d5

---
+++ assets/js/drop-down.js
@@ -0,0 +1,30 @@
+let column_link = document.querySelectorAll(".link-parent");
+column_link = Array.from(column_link);
+column_link.forEach((item) => {
+  const subdrop = item.nextElementSibling;
+  const classsubdrop = subdrop.classList;
+  console.log(classsubdrop);
+
+  item.onclick = () => {
+    if (classsubdrop.contains("menu-column__item--active")) {
+      subdrop.classList.remove("menu-column__item--active");
+    } else {
+      subdrop.classList.add("menu-column__item--active");
+    }
+  };
+});
+
+let navbar_link = document.querySelectorAll(".navbar__link");
+navbar_link = Array.from(navbar_link);
+navbar_link.forEach((item) => {
+  const dropdown = item.nextElementSibling;
+  const classdropdown = dropdown.classList;
+  item.onclick = () => {
+    console.log(dropdown);
+    if (classdropdown.contains("show")) {
+      dropdown.classList.remove("show");
+    } else {
+      dropdown.classList.add("show");
+    }
+  };
+});
